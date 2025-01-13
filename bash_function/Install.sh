ebd40ef610016141a547766151743aaa1ede74b5
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-01-13 12:48:26 +0530

    Update Install.sh

--- bash_function/Install.sh
+++ bash_function/Install.sh
@@ -54,7 +54,7 @@ purge_terraform() {
 # Function to install Terraform
 install_terraform() {
     echo "Installing Terraform..."
-    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
     sudo apt update && sudo apt install terraform -y
     echo "Terraform installation complete."
